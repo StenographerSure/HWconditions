f408d2af9fa692f75a7583c58b13a7065d07b75b
StenographerSure <glibockiy@inbox.ru>
2024-10-09 21:53:12 +0400

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //задача 1
+        System.out.println("Задача 1");
+        int age = 18;
+        System.out.println("Если возраст человека равен" + age +", то ");
+        if (age >= 18){
+            System.out.println("Человеку 18 лет или больше");
+        }
+        else {
+            System.out.println("Возраст совершеннолетия еще не наступил, нужно немного подождать.");
+        }
+
+        //задача 2
+        System.out.println("Задача 2");
+        int temp = 10;
+        System.out.println("На улице " + temp + " градусов");
+        if(temp <= 5){
+            System.out.println("На улице холодно, нужно надеть шапку");
+        }
+        else{
+            System.out.println("Сегодня тепло, можно идти без шапки");
+        }
+
+        //задача 3
+        System.out.println("Задача 3");
+
+        int currentSpeed = 65;
+        System.out.println("Если скорость " + currentSpeed + ", то");
+
+        if(currentSpeed <= 60){
+            System.out.println("можно ездить спокойно");
+        }
+        else{
+            System.out.println("придется заплатить штраф");
+        }
+
+        //задача 4
+        System.out.println("Задача 4");
+
+        int age_2 = 20;
+        if (age_2>2){
+
+        }
+
+
     }
 }
